492fd4f9ff7c5be7ab4b6c048f1e5160eb346935
Elliot <5415760+elliot-gh@users.noreply.github.com>
2026-03-18 00:18:32 -0700

    Increase debounce

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/elliot-gh/config.h
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/elliot-gh/config.h
@@ -30,7 +30,7 @@
 #define QMK_KEYS_PER_SCAN 12
 
 #undef DEBOUNCE
-#define DEBOUNCE 4
+#define DEBOUNCE 5
 
 #define RGB_MATRIX_SLEEP
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CUSTOM_indicator_only
